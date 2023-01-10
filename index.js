9ac79e54c5543aef5f0a2bcce773c2056f8e9db3
jamesackies <jamesackies8@gmail.com>
2023-01-10 14:32:46 -0500

    commit tree add0b2ae071b0077e82bcdc66a2b35e8053dfc01

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Follow along with the examples here
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }console.log(say("Hello", "Liz"));
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }console.log(say("Howdy", "partner"));
+  
